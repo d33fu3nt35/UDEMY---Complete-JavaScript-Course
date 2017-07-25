8056ee0c9ddc13d7066fe57a37f0ddb6fa126394
Daniel De La Fuentes <d33fu3nt35@gmail.com>
2017-07-24 20:45:02 -0500

    Section 2, Lecture 10 - Brief breakdown about Variable mutation and Type Coercion. Also wen't over basic Prompt Box & Alert Box Syntax.

--- Javascript/Script.js
+++ Javascript/Script.js
@@ -2,6 +2,8 @@
  * Created by daniel on 7/24/17.
  */
 
+/* Lecture: Variables
+
 var name = 'Daniel ';
 console.log(name);
 
@@ -13,3 +15,32 @@ console.log(age);
 
 var legalAge = true;
 console.log(legalAge)
+
+*/
+
+// Lecture: Variables 2
+
+var name = 'Daniel';
+var age = 21;
+
+console.log(name + ' ' + age);
+console.log(age + age);
+
+var job, isMarried;
+
+console.log(job);
+
+job = 'Full Stack Web Developer';
+isMarried = false;
+
+console.log(name + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried + '. ');
+
+age = 'Twenty One';
+job = 'Warehouse Worker';
+
+console.log(name + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried + '. ');
+
+var lastName = prompt('What is the last name?');
+console.log(lastName);
+
+alert(name + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried + '. ');
