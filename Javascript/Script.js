3697f91a445f64612e33f350e239dfe25d8031c3
Daniel De La Fuentes <d33fu3nt35@gmail.com>
2017-07-24 20:01:52 -0500

    Section 2, Lecture 9 - Wen't over creating variables and the primitive data types such as Strings, Integers, Booleans, Undefined, & NULL. Also lectured about JavaScript dynamic typing as well as the importance of console.logging & how although right now it might not feel important, it comes into play later down the line for Debugging purposes.

--- Javascript/Script.js
+++ Javascript/Script.js
@@ -2,4 +2,14 @@
  * Created by daniel on 7/24/17.
  */
 
-console.log('Hello World');
+var name = 'Daniel ';
+console.log(name);
+
+var lastName = 'De La Fuentes';
+console.log(name + lastName);
+
+var age = 21;
+console.log(age);
+
+var legalAge = true;
+console.log(legalAge)
